d60de709199b5c03b4c77c0d396f8ee1c1032562
Mayank-44 <37243315+Mayank-44@users.noreply.github.com>
2018-03-12 21:12:19 +0530

    compare two linked list

--- LL_compare.cpp
+++ LL_compare.cpp
@@ -1,17 +1,14 @@
 /*
   Compare two linked lists A and B
   Return 1 if they are identical and 0 if they are not. 
-  Node is defined as 
+  Node is defined as */
   struct Node
   {
      int data;
      struct Node *next;
   }
-*/
 int CompareLists(Node *headA, Node* headB)
 {
-  // This is a "method-only" submission. 
-  // You only need to complete this method 
     int a=0,b=0;
     Node *temp1=headA,*temp2=headB;
     while(temp1!=NULL)
