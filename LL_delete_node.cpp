b693339a836271922d5d15e953b1b2fcfb0147bc
Mayank-44 <37243315+Mayank-44@users.noreply.github.com>
2018-03-12 20:41:22 +0530

    delete any specific node

---
+++ LL_delete_node.cpp
@@ -0,0 +1,26 @@
+/*
+  Delete Node at a given position in a linked list 
+  Node is defined as 
+  struct Node
+  {
+     int data;
+     struct Node *next;
+  }
+*/
+Node* Delete(Node *head, int position)
+{
+    Node *temp=head;
+  // Complete this method
+    if(position==0)
+    head=head->next;
+    else
+    {
+        for(int i=1;i<position;i++)
+        {
+            temp=temp->next;
+        }
+        temp->next=temp->next->next;
+    }
+    return head;
+}
+
