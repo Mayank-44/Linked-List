32ae055b8fdb5ff06c53f9582b6c5341f165591e
Mayank-44 <37243315+Mayank-44@users.noreply.github.com>
2018-03-12 20:37:21 +0530

    insert element at the tail of link list

---
+++ LL_insert_at_tail.cpp
@@ -0,0 +1,32 @@
+/*
+  Insert Node at the end of a linked list 
+  head pointer input could be NULL as well for empty list
+  Node is defined as 
+  struct Node
+  {
+     int data;
+     struct Node *next;
+  }
+*/
+Node* Insert(Node *head,int data)
+{
+  // Complete this method
+    Node *temp=new Node();
+    temp->data=data;
+    temp->next=NULL;
+    if(head)
+    {
+        Node *p;
+        p=head;
+        while(p->next!=NULL)
+        {
+            p=p->next;
+        }
+        p->next=temp;    
+    }else
+    {
+        head=temp;
+    }
+   return head;
+}
+
