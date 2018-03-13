7d18db11b96db923c14e64133381d68ee994870d
Mayank-44 <37243315+Mayank-44@users.noreply.github.com>
2018-03-13 23:39:56 +0530

    find if cycle exist in Linked List

---
+++ LL_find_if_cycle_exist.cpp
@@ -0,0 +1,31 @@
+/*
+Detect a cycle in a linked list. Note that the head pointer may be 'NULL' if the list is empty.
+
+A Node is defined as: 
+    struct Node {
+        int data;
+        struct Node* next;
+    }
+*/
+
+bool has_cycle(Node* head) {
+    // Complete this function
+    // Do not write the main method
+    int count=100,c=0;
+    if(head==NULL || head->next==NULL)
+        return false;
+    else
+    {
+        Node *prev=head,*ahead=head->next;
+        while(ahead!=NULL && c<count)
+        {
+            prev=prev->next;
+            ahead=ahead->next;
+            c++;
+        }
+        if(c<count) 
+        return false;
+    }
+    return true;
+}
+
